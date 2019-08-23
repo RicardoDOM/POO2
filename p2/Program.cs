b8d8dd988a38c784bd920a1167e1b58b7f60a2a5
AlbertoRaCa <47792343+AlbertoRaCa@users.noreply.github.com>
2019-08-23 12:47:23 -0700

    Update Program.cs

--- p2/Program.cs
+++ p2/Program.cs
@@ -6,7 +6,7 @@ namespace p2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("HOLA TEC");
         }
     }
 }
